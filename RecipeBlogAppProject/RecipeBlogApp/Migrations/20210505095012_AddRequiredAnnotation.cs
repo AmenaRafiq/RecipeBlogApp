bbe0c2665035acfabc1598ab116e3a4c6030b7ed
AmenaRafiq <amena.rafiq@avanade.com>
2021-05-05 10:51:29 +0100

    Add [required] annotations to DB fields

---
+++ RecipeBlogAppProject/RecipeBlogApp/Migrations/20210505095012_AddRequiredAnnotation.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBlogApp.Migrations
+{
+    public partial class AddRequiredAnnotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Method",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ingredients",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Method",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ingredients",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+    }
+}
--- RecipeBlogAppProject/RecipeBlogLibrary/Models/Recipe.cs
+++ RecipeBlogAppProject/RecipeBlogLibrary/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,14 @@ namespace RecipeBlogLibrary.Models
     public class Recipe
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
         public string ImageURL { get; set; }
+        [Required]
         public string Ingredients { get; set; }
+        [Required]
         public string Method { get; set; }
+        [Required]
         public int Servings { get; set; }
 
     }
