7ef00c957dca9b9c981a73b82e65d8165710b5f8
AmenaRafiq <amena.rafiq@avanade.com>
2021-05-05 09:54:40 +0100

    Add new ASP.NET MVC app project

---
+++ RecipeBlogAppProject/RecipeBlogApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeBlogApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
