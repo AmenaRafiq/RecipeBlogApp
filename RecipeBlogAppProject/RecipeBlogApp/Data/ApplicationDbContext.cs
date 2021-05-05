c5855bc4fba186d181759a8250b1fe8356213c76
AmenaRafiq <amena.rafiq@avanade.com>
2021-05-05 10:41:32 +0100

    Add connection to Azure DB and initialise DB

---
+++ RecipeBlogAppProject/RecipeBlogApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeBlogLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBlogApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipeCard> RecipeCards { get; set; }
+    }
+}
---
+++ RecipeBlogAppProject/RecipeBlogApp/Migrations/20210505094002_DBInit.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBlogApp.Migrations
+{
+    public partial class DBInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    ImageURL = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Ingredients = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Method = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Servings = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeCards",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RecipeID = table.Column<int>(type: "int", nullable: true),
+                    Title = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    ImageURL = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeCards", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RecipeCards_Recipes_RecipeID",
+                        column: x => x.RecipeID,
+                        principalTable: "Recipes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeCards_RecipeID",
+                table: "RecipeCards",
+                column: "RecipeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecipeCards");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
