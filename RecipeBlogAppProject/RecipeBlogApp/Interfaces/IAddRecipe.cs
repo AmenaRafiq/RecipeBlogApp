883a14f91c6fe0f17a221bfedf9dc57436687624
AmenaRafiq <amena.rafiq@avanade.com>
2021-05-07 17:01:26 +0100

    Add interfaces for testing, add test project

---
+++ RecipeBlogAppProject/RecipeBlogApp/Interfaces/IAddRecipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBlogApp.Interfaces
+{
+    interface IAddRecipe
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Ingredients { get; set; }
+        public string Method { get; set; }
+        public int Servings { get; set; }
+    }
+}
---
+++ RecipeBlogAppProject/RecipeBlogApp/Interfaces/IRecipeCard.cs
@@ -0,0 +1,16 @@
+﻿using RecipeBlogApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeBlogApp.Interfaces
+{
+    interface IRecipeCard
+    {
+        public int ID { get; set; }
+        //public virtual Recipe Recipe { get { } set { } }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
